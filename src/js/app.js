a7694ee9d3a0f084fcfdd1667a024aa0a4dd32ea
erdoszrf <erdoszrf@qq.com>
2018-04-25 09:29:08 +0800

    基础样式

---
+++ src/js/app.js
@@ -0,0 +1,17 @@
+let app = new Vue({
+  el:'#app',
+  data:{
+    resume:{
+      name:'',
+      age:'',
+      sex:'',
+      location:'',
+      phone:'',
+      email:'',
+      wechat:''
+    },
+    skills:{
+      [skill:'',]
+    }
+  }
+})
