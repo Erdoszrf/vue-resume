f317f01ea4c7d51e450aa45740d8d751899a397a
erdoszrf <erdoszrf@qq.com>
2018-05-04 16:23:19 +0800

    登录注册

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+Vue.com
 let app = new Vue({
   el:'#app',
   data:{
@@ -9,6 +10,52 @@ let app = new Vue({
       phone:'',
       email:'',
       wechat:''
+    },
+    skills:[
+      {},
+      {},
+    ],
+    signup:{
+      username:'',
+      password:'',
+    },
+    login:{
+      username:'',
+      password:''
+    },
+    loginTaggle:false,
+    signUpTaggle:false
+  },
+  methods:{
+    loginShow:function(){
+      this.loginTaggle = !(this.loginTaggle)
+    },
+    signUpShow:function(){
+      this.signUpTaggle = !(this.signUpTaggle)
+    },
+    gologin:function(){
+      AV.User.logIn(this.login.username, this.login.password).then(function (loginedUser) {
+        console.log(loginedUser);
+        app.loginTaggle = false
+      }, function (error) {
+        console.log(error)
+      });
+    },
+    gosignUp:function(){
+      // 新建 AVUser 对象实例
+      var user = new AV.User();
+      // 设置用户名
+      user.setUsername(this.username);
+      // 设置密码
+      user.setPassword(this.password);
+      // 设置邮箱
+      user.setEmail(this.username);
+      user.signUp().then(function (loginedUser) {
+          console.log(loginedUser);
+          app.signUpTaggle = false
+      }, function (error) {
+        console.log(error)
+      });
     }
   }
 })
---
+++ src/js/avinit.js
@@ -0,0 +1,8 @@
+var APP_ID = 'zfSYtBI4d4lxjauxK1pyRoT0-gzGzoHsz';
+var APP_KEY = '9kAL2YIbBwDg4quOpx0PXAV7';
+
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
+ 
